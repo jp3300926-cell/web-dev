0df2a47a34f9f34b15577cc10347b463b78819d8
john paul <jp3300926@gmail.com>
2025-09-27 13:12:55 +0800

    new

--- project/function.js
+++ project/function.js
@@ -3,12 +3,12 @@ let menubox = document.getElementById("popup-menu-box");
 let changeicon= document.getElementById("change-icon");
 
 function popup_menu() {
-  if (popupmenu.style.width === "2vw") {
-    popupmenu.style.width = "10.1vw";
-    menubox.style.width = "8.1vw";
-    changeicon.innerHTML = "×";
+  if (popupmenu.style.width === "5vw") {
+    popupmenu.style.width = "23vw";
+    menubox.style.width = "18vw";
+    changeicon.innerHTML = "X";
   } else {
-    popupmenu.style.width = "2vw";
+    popupmenu.style.width = "5vw";
     menubox.style.width = "0";
     changeicon.innerHTML = "&#9776";
   }
